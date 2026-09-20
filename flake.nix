da42582830cbcb4cb5872d704f59dc287cd868d9
Kenichi Kamiya <kachick1@gmail.com>
2026-09-20 12:08:48 +0900

    Fix and run Nix checkPhase (#42)
    * Update cargoHash in flake.nix
    
    * Use cargoLock.lockFile over cargoHash
    
    * Fix and run Nix checkPhase again

--- flake.nix
+++ flake.nix
@@ -23,10 +23,7 @@
           pname = cargo.package.name;
           version = cargo.package.version;
           src = self;
-          cargoHash = "sha256-VOL+PShh/NCNJIPLFoD2bLuDtWdOBWBeoX7BqTk8++o=";
-
-          # Skip tests - integration tests need the built binary which isn't available in sandbox
-          doCheck = false;
+          cargoLock.lockFile = ./Cargo.lock;
         };
 
         runtimeEnv = with pkgs; [
